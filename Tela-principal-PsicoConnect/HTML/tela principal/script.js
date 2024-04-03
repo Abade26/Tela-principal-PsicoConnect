65285908b25be8223be3719b4db9e021510d1ddf
Abade26 <140136551+Abade26@users.noreply.github.com>
2024-04-03 16:21:59 -0300

    Delete Tela-principal-PsicoConnect/HTML directory

--- Tela-principal-PsicoConnect/HTML/tela principal/script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function() {
-    $('#mobile_btn').click(function() {
-        $('#mobile_menu').toggleClass('active');
-        $(this).find('i').toggleClass('fa-bars fa-x');
-    });
-});
