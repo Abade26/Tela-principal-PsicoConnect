c5e95475c67e3eeb376ef3a9aa12fd2bd8a89ed9
Abade26 <140136551+Abade26@users.noreply.github.com>
2024-04-04 16:34:24 -0300

    Html e css em  produção

--- HTML/tela principal/script.js
+++ HTML/tela principal/script.js
@@ -1,7 +1,14 @@
-
 $(document).ready(function() {
     $('#mobile_btn').click(function() {
         $('#mobile_menu').toggleClass('active');
         $(this).find('i').toggleClass('fa-bars fa-x');
+    });
+     
+    const sections = $('section');
+    const navItms = $('.nav-item');
+
+    $(window).on('scroll', function() {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
     });
 });
