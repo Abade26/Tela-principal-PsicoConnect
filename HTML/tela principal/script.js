f299603fa965183ca43f4939c53f52d7b3e494ae
Abade26 <140136551+Abade26@users.noreply.github.com>
2024-04-19 15:56:34 -0300

    popup

--- HTML/tela principal/script.js
+++ HTML/tela principal/script.js
@@ -12,3 +12,15 @@ $(document).ready(function() {
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
     });
 });
+
+// Função para abrir o popup
+function openPopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+}
+
+// Função para fechar o popup
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
